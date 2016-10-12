834219e1936f98f96e9441a17d50fea721263619
khiemlam <khiem@reifyhealth.com>
2016-10-11 20:31:26 -0400

    Report ex-data as extra data

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject raven-clj "1.4.2"
+(defproject com.github.reifyhealth/raven-clj "1.4.3"
   :description "Sentry clojure client"
   :url "https://github.com/reifyhealth/raven-clj"
   :license {:name "Eclipse Public License"
@@ -6,4 +6,5 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [cheshire "5.0.1"]
                  [clj-http "3.0.1"]
-                 [prone "1.0.1"]])
+                 [prone "1.0.1"]]
+  :deploy-repositories [["clojars" {:sign-releases false}]])
